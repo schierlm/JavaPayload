7a314b23d2f04e3fd4d35bb336c88bc36c12523d
Michael Schierl <schierlm@gmx.de>
2012-04-28 18:36:11 +0000

    Add GrowSize dynstager to test how builders/dynstagers/crypters handle huge stagers
    git-svn-id: https://javapayload.svn.sourceforge.net/svnroot/javapayload/trunk@70 966532e4-22f8-445e-ae78-778a4fe0991c

---
+++ JavaPayload/test/javapayload/handler/dynstager/GrowSize.java
@@ -0,0 +1,61 @@
+/*
+ * Java Payloads.
+ * 
+ * Copyright (c) 2012 Michael 'mihi' Schierl
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *   
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *   
+ * - Neither name of the copyright holders nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *   
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND THE CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDERS OR THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package javapayload.handler.dynstager;
+
+import javapayload.Parameter;
+
+public class GrowSize extends DynStagerHandler {
+
+	public GrowSize() {
+		super("Increase the size of a stager", true, true,
+				"Useful for testing how dynstagers/builders/crypters handle huge class files");
+	}
+
+	public Parameter getExtraArg() {
+		return new Parameter("COUNT", false, Parameter.TYPE_NUMBER, "Number of 50KB blocks to add");
+	}
+
+	public Parameter[] getParameters() {
+		return new Parameter[0];
+	}
+
+	public boolean isDynstagerUsableWith(DynStagerHandler[] dynstagers) {
+		return true;
+	}
+
+	public String getTestExtraArg() {
+		return "5";
+	}
+}
